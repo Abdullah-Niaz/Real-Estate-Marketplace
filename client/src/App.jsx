599a17f66b1c1f4ce67aba3da13a70db1b55fe97
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-10-21 00:04:46 +0500

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css'
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
 
+import About from "./pages/about";
 function App() {
   return <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<Home/>} />
+      <Route path="/sign-in" element={<Signin/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
     </Routes>
   </BrowserRouter>
 }
