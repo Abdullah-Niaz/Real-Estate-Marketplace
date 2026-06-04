e2f8fbb5ebec7b0cf1905b8d2051d20be30b3111
Abdullah Niaz <abdullahniaz423@gmail.com>
2026-06-04 12:15:27 +0500

    singin/singup config done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import errorHandler from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 
 export const singup = async (req, res, next) => {
@@ -22,6 +23,41 @@ export const singup = async (req, res, next) => {
     } catch (error) {
         next(errorHandler(500, "Error creating user"));
     }
-
-
 }
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+
+        const validUser = await User.findOne({ email });
+
+        if (!validUser) {
+            return next(errorHandler(404, "User not found"));
+        }
+
+        const validPassword = bcryptjs.compareSync(
+            password,
+            validUser.password
+        );
+
+        if (!validPassword) {
+            return next(errorHandler(401, "Wrong Credentials!"));
+        }
+
+        const token = jwt.sign(
+            { id: validUser._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        );
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+};
